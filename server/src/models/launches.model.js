e6c3a6cb72e625d7434a3f47d6db420a25f01948
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-16 18:29:18 -0600

    The create launch functionality was completed.

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,10 +4,10 @@ let latestFlightNumber = 100;
 
 const launch = {
   flightNumber: 100,
-  mission: "",
-  rocket: "",
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
