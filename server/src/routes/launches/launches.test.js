e853071eb7501b58b19cd8179a54022f7e0ace40
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-20 18:26:02 -0600

    The backend test were updated.

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app = require("../../app");
-const { mongoConnect } = require("../../services/mongo");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
 describe("Launches API", () => {
   beforeAll(async () => {
@@ -9,6 +9,10 @@ describe("Launches API", () => {
     } catch (error) {
       console.error("Error conectando: ", error);
     }
+  });
+
+  afterAll(async () => {
+    await mongoDisconnect();
   });
 
   describe("Test GET /launches", () => {
@@ -24,20 +28,20 @@ describe("Launches API", () => {
     const completeLaunchData = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
       launchDate: "January 4, 2028",
     };
 
     const launchDataWithoutDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
     };
 
     const launchDataWithInvalidDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
       launchDate: "zoot",
     };
 
@@ -51,7 +55,7 @@ describe("Launches API", () => {
       expect(response.body).toMatchObject({
         mission: "USS Enterprise",
         rocket: "NCC 1701-D",
-        target: "Kepler-186 f",
+        target: "Kepler-62 f",
       });
 
       const requestDate = new Date(completeLaunchData.launchDate).valueOf();
